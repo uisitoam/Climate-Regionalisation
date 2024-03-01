91fe5743271d864af6746a5d9fb002944a933e36
Luis Ardevol <76615495+uisitoam@users.noreply.github.com>
2024-03-01 16:49:05 +0000

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,78 @@
+import numpy as np 
+import time as timeMod
+from bot import bot_texter, timecalc
+from reader import reader
+from datafunctions import extractData
+from trainfunctions import execution
+from nets import temperatureModel, precipModel, gaussianLoss, bernouilliGammaLoss
+from plots import boxplots, get_colormap, mapeo
+
+
+start1 = timeMod.time()
+
+# reading the data files (from 1982-01-05 to 2019-12-31)
+tDate, tMean, tLong, tLat = reader("Datos/data_plcwrf_t2mean_1982_2019.nc")
+pDate, precip, pLong, pLat = reader("Datos/data_plcwrf_pr_1982_2019.nc")
+
+# reading the data from era5 file (from 1982-01-05 to 2019-12-31)
+time, longitude, latitude, level, z, q, t, u, v = reader("Datos/data_era5ann_1982_2019.nc", 
+                                                         era5=True)
+
+fin1 = timeMod.time()
+timecalc(start1, fin1, 'Tiempo en leer los archivos', True)
+
+
+times = [time, time.index('20100101'), time.index('20191231')]
+
+era5data = [z, q, t, u, v]
+
+
+start2 = timeMod.time()
+
+#temperature data (in Kelvin)
+tempParams = [60, 128, 5]
+
+tempData, tempStore, tempTime = extractData(era5data, tMean, times, "Temperature")
+
+tempPred, tempMetricas = execution(temperatureModel, gaussianLoss, tempData, 
+                                  tempStore, tempTime, tempParams)
+
+fin2 = timeMod.time()
+timecalc(start2, fin2, 'Tiempo para el modelo de temperatura (60 epochs, batch size de 128 y 5 repeticiones)', True)
+
+tCols = ['Mean Bias', 'P2 Bias', 'P98 Bias', 'R (Pearson)', 'std Ratio', 'RMSE', 'Bias WAMS', 'Bias CAMS']
+
+boxplots(tempMetricas, tCols, 
+         ['Temperature', f'./Resultados/Temperatura/plots/{tempParams[0]}_epochsTempMetrics.pdf'])
+
+mapeo(tempPred, [np.shape(tempPred)[0], 68, 158], 
+      [np.min(tLong), np.max(tLong), np.min(tLat), np.max(tLat)],
+      [get_colormap('t'), './Resultados/Temperatura/plots/temperatureMap.pdf'], 
+      [tempTime[1], '20120307'])
+
+
+start3 = timeMod.time()
+
+#precipitation data 
+precipParams = [120, 128, 5]
+
+precipData, precipStore, precipTime = extractData(era5data, precip - 1, times, "Precipitation")
+
+precipPred, precipMetricas = execution(precipModel, bernouilliGammaLoss, 
+                                       precipData, precipStore, precipTime, 
+                                       precipParams)
+
+fin3 = timeMod.time()
+timecalc(start3, fin3, 'Tiempo para el modelo de precipitaciones determinista (120 epochs, batch size de 128 y 5 repeticiones)', True)
+
+# used metrics
+pCols = ['Mean Bias', 'P98 Bias', 'R (Spearman)', 'RMSE (Wet days)', 'Bias WetAMS', 'Bias_DryAMS']
+
+boxplots(precipMetricas, pCols, 
+         ['Precipitation', f'./Resultados/Precipitacion/plots/{precipParams[0]}_epochsPrecipMetrics.pdf'])
+
+mapeo(precipPred, [np.shape(precipPred)[0], 68, 158], 
+      [np.min(pLong), np.max(pLong), np.min(pLat), np.max(pLat)], 
+      [get_colormap('p'), './Resultados/Precipitacion/plots/precipitationMap.pdf'], 
+      [precipTime[1], '20120307'])
+
---
+++ python/plots.py
@@ -0,0 +1,335 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+import cartopy.feature as cpf
+import cartopy.crs as ccrs
+from nets import temperatureModel, precipModel
+
+colores = ['#FF0000', '#FF9B00', '#F5DE00', '#51FF53', '#00A902', '#00FF8F', 
+           '#00F5E6', '#00B2FF', '#0032FF', '#B900FF', '#8700FF', '#FF86F6', 
+           '#EA00D9', '#B00070', '#A4A4A4']
+
+def learningCurve(ejey, ep, savePath):
+    """
+    This functions plots the leraning curves of the models. 
+
+    Parameters
+    ----------
+    ejey : list
+        List containing the training and the validation loss as two different 
+        NumPy arrays.
+    ep : int
+        Epochs
+    savePath : str
+        Path where the plot will be saved.
+
+    Raises
+    ------
+    ValueError
+        If `ejey` do not contain 2 NumPy arrays.
+    PermissionError
+        Insufficient permission to save the plot on the specified path.
+    FileNotFoundError
+        If the specified save path does not exist and is not writable.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    if len(ejey) != 2 or not all(isinstance(i, np.ndarray) for i in ejey):
+        raise ValueError("ejey must be a list of 2 NumPy arrays.")
+
+    fig, ax = plt.subplots(1, 1, figsize=(9,5))
+
+    ejex = np.arange(1, ep + 1)
+
+    ax.plot(ejex, ejey[0], color=colores[5], label='Training Loss')
+    ax.plot(ejex, ejey[1], color=colores[9], label='Validation Loss')
+    ax.set(xlabel='Epochs', ylabel='Loss', title='Training and Validation Loss')
+    ax.legend()
+    
+    if savePath: # Validate directory or file path
+    
+        try:
+            with open(savePath, 'x') as f:
+                pass  # Create an empty file to indicate write permission
+                
+        except PermissionError:
+            raise PermissionError(f"Insufficient permissions to save the plot to '{savePath}'.")
+        
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Save path '{savePath}' not found.")
+        
+        fig.savefig(savePath)
+    
+    
+    return 
+
+
+
+
+
+def boxplots(metrics, metricLabels, params):
+    """
+    Creates a boxplot of given metrics.
+
+    Parameters
+    ----------
+    metrics : np.ndarray
+        NumPy array with the metrics being evaluated in that model.
+    metricLabels : list
+        List containing the names of the previous metrics. 
+    params : list
+        List of strings with the model being evaluated and the saving path 
+        for the plot.
+
+    Raises
+    ------
+    TypeError
+        If the title parameter in `params` is not "Temperature" or "Precipitation".
+    ValueError
+        If the dimensions of `metrics` and `metricLabels` don't match.
+    PermissionError
+        Insufficient permissions to save the plot to specified path.
+    FileNotFoundError
+        If the specified save path does not exist and is not writable.
+
+    Returns
+    -------
+    Matplotlib figure object
+
+    """
+    
+    if not isinstance(metrics, np.ndarray):
+        raise TypeError("metrics must be a NumPy array")
+    
+    if not all(isinstance(i, str) for i in metricLabels):
+        raise TypeError("metricLabels must be a list of only strings")
+    
+    if not all(isinstance(i, str) for i in params):
+        raise TypeError("params must be a list of only strings")
+
+    if np.shape(metrics)[0] != len(metricLabels):
+        raise ValueError("metrics and metricLabels must have the same length.")
+        
+    if params[0] not in ('Temperature', 'Precipitation'):
+        raise TypeError("Title must be `Temperature` or `Precipitation`")
+        
+
+    def approx(value, expect):
+      resto = value % expect
+      
+      if value < 0:
+        return value - resto
+    
+      else:
+        return value + (expect - resto)
+    
+    data = [element for element in metrics]
+    
+    fig, ax = plt.subplots(1, 1, figsize=(9, 5))
+    
+    ax.boxplot(data, vert=False, showfliers = False, 
+               notch = True, labels=metricLabels)
+    
+    ax.set(title=params[0])
+    ax.set_yticklabels(metricLabels)
+    
+    q1 = np.nanquantile(metrics, 0.25, axis=1)
+    q3 = np.nanquantile(metrics, 0.75, axis=1)
+    iqr = q3 - q1
+    
+    if params[0] == 'Temperature':
+        step = 0.5
+        xticks = np.arange(approx(np.min(q1 - 1.5*iqr), step) - step, 
+                           approx(np.max(q3 + 1.5*iqr), step) + step, step)
+        
+        xlabels = [f'{x:1.1f}' for x in xticks]
+        ax.set_xticks(xticks, minor=True, labels=xlabels)
+        ax.tick_params(axis='x', labelsize=10)
+        
+    else:
+        step = 10
+        xticks = np.arange(approx(np.min(q1 - 1.5*iqr), step) - step, 
+                           approx(np.max(q3 + 1.5*iqr), step) + step, step)
+        
+        xlabels = [f'{x:1.1f}' for x in xticks]
+        ax.set_xticks(xticks, labels=xlabels, minor=True)
+        ax.tick_params(axis='x', which='both', labelsize=8)
+    
+    plt.subplots_adjust(top=0.935, bottom=0.175, left=0.16, right=0.92, 
+                        hspace=0.2, wspace=0.2)
+    
+    try:
+        with open(params[1], 'x') as f:
+            pass  # Create an empty file to indicate write permission
+    
+    except PermissionError:
+        raise PermissionError(f"Insufficient permissions to save the plot to '{params[1]}'.")
+    
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Save path '{params[1]}' not found.")
+
+    fig.savefig(params[1])
+    
+    return 
+
+
+
+
+
+def get_colormap(var):
+    """
+    Return a colormap for the given variable.
+
+    Parameters
+    ----------
+    var : str
+        Must be `t` for Temperture and `p` for Precipitation.
+
+    Raises
+    ------
+    TypeError
+        If var is not 't' or 'p'.
+
+    Returns
+    -------
+    Matplotlib LinearSegmentedColormap object.
+
+    """
+    
+    if var not in ('t', 'p'):
+        raise TypeError("Variable must be 't' for temperature or 'p' for precipitation.")
+    
+    colormaps = {'t': [(0.5, 0.0, 0.5), (0.2, 0.0, 0.4), (0.0, 0.0, 1.0), 
+                       (0.0, 0.8, 1.0), (0.0, 1.0, 0.0), (1.0, 1.0, 0.0),
+                       (1.0, 0.5, 0.0), (1.0, 0.0, 0.0), (0.1, 0.0, 0.0)], 
+                 'p': [(1.0, 1.0, 1.0), (0.8, 0.9, 1.0), (0.6, 0.8, 1.0), 
+                       (0.4, 0.7, 1.0), (0.2, 0.5, 1.0), (0.0, 0.3, 0.8), 
+                       (0.0, 0.1, 0.6), (0.0, 0.0, 0.2)]
+                 }
+    
+    return LinearSegmentedColormap.from_list("full_spectrum", colormaps[var])
+
+    
+
+
+
+
+def mapeo(data, dataDim, dataExt, params, time, seamask=False, imgExt=None):
+    """
+    Make a map plot of the given area for the specified variable.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        Data (temperature or rain amount) to be plot over the map.
+    dataDim : list
+        Dimensions of the given data like (time, latitude, longitude).
+    dataExt : list
+        Extremal values of latitude and longitude of the data.
+    params : list
+        Contains the colormap and the save path.
+    time : list
+        Contains all dates with available data and the exact day to be plotted.
+    seamask : bool, optional
+        Add a white mask to the sea. The default is False.
+    imgExt : list, optional
+        Extent of the plotted image, by default is the same as the data 
+        extension. The default is None.
+
+    Raises
+    ------
+    TypeError: 
+        If any of the following conditions are met:
+            - `data` is not a numpy array.
+            - `dataDim` is not a list of length 3.
+            - `dataExt` is not a list of length 4.
+            - `params` is not a string list of length 2.
+            - `colorMap` in `params` is not a matplotlib colormap object.
+            - `savePath` in `params` is not a string.
+    ValueError
+        If the dimensions of `data` and `dataDim` are not compatible.
+    PermissionError
+        Insufficient permissions to save the plot to specified path.
+    FileNotFoundError
+        If the specified save path does not exist and is not writable.
+
+    Returns
+    -------
+    Matplotlib figure object.
+
+    """
+    
+
+    if not isinstance(data, np.ndarray):
+        raise TypeError("Data must be a NumPy array.")
+  
+    if not isinstance(dataDim, list) or len(dataDim) != 3 or not all(isinstance(i, int) for i in dataDim):
+        raise ValueError("dataDim must be a list of three integers.")
+  
+    if not isinstance(dataExt, list) or len(dataExt) != 4 or not all(isinstance(i, (int, float)) for i in dataExt):
+        raise TypeError("dataExt must be a list of 4 numbers (int or floats).")
+  
+    if not isinstance(params, list) or len(params) != 2:
+        raise TypeError("Params must be a tuple of length 2 containing (colormap, save path).")
+        
+    if not isinstance(time, list) or len(time) != 2:
+        raise TypeError("time must be a list containing (Dates, given date)")
+        
+    if int(time[1]) < int(time[0][0]) or time[1] > int(time[0][-1]):
+        raise TypeError(f'Date must be contain between {int(time[0][0])} and {int(time[0][-1])}.')
+    
+    if time[1] not in time[0]:
+        raise TypeError(f'Not available data for {int(time[1])}.')
+
+    
+    colorMap, savePath = params
+    data = np.reshape(data, (dataDim[0], dataDim[1], dataDim[2]))
+    data = data[time[0].index(time[1]), :, :]
+    
+    fig = plt.figure(figsize=(9, 5))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.coastlines(resolution='10m', color='black', linewidth=1)
+    ax.set_extent(dataExt)
+    
+    if not imgExt:
+        imgExt = dataExt
+        
+    #img_extent = [np.min(lons), np.max(lons), np.min(lats), np.max(lats)]
+    im = ax.imshow(data[:, :], extent=imgExt, cmap=colorMap)
+    
+    if seamask:
+        ocean = cpf.NaturalEarthFeature(category= 'physical', 
+                                        name='ocean', scale='10m')
+        
+        ax.add_feature(ocean, zorder=100, edgecolor='k')
+    
+    plt.colorbar(im, shrink=0.5, format='%.0f', cmap=colorMap)
+    
+    """
+    Ensures that the file is ready to be written without the risk 
+    of overwriting an existing one.
+    """
+    try:
+        with open(savePath, 'x') as f: 
+            pass  # Create an empty file to indicate write permission
+    
+    except PermissionError:
+        raise PermissionError(f"Insufficient permissions to save the plot to '{savePath}'.")
+    
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Save path '{savePath}' not found.")
+
+    fig.savefig(savePath)
+
+    return 
+
+
+
+
+
+
+
