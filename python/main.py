3c629540e5f880a09ea757e9058d667c2df7f2ac
Luis Ardevol <76615495+uisitoam@users.noreply.github.com>
2024-03-15 17:03:09 +0000

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,113 @@
+import numpy as np 
+import time as timeMod
+from bot import timecalc, bot_texter, printeo
+from reader import reader
+from datafunctions import extractData, unmask, extractMask
+from trainfunctions import execution
+from nets import temperatureModel, precipModel, gaussianLoss, bernouilliGammaLoss
+from plots import boxplots, get_colormap, mapeo, animation
+from tqdm import tqdm
+
+
+# reading the data files (from 1982-01-05 to 2019-12-31)
+tDate, tMean, tLong, tLat = reader("Datos/data_plcwrf_t2mean_1982_2019.nc")
+pDate, precip, pLong, pLat = reader("Datos/data_plcwrf_pr_1982_2019.nc")
+pDate2, precip2, pLong2, pLat2 = reader("Datos/data_plcwrf_pr_1982_2019_masked.nc")
+
+# reading the data from era5 file (from 1982-01-05 to 2019-12-31)
+time, longitude, latitude, level, z, q, t, u, v = reader("Datos/data_era5ann_1982_2019.nc", 
+                                                         era5=True)
+
+times = [time, time.index('20100101'), time.index('20191231')]
+
+era5data = [z, q, t, u, v]
+
+
+"""
+# TEMPERATURE
+tempParams = [500, 128, 5]
+
+# temperature data (in Kelvin)
+tempData, tempStore, tempTime = extractData(era5data, tMean, times, "Temperature")
+
+# temperature results
+start2 = timeMod.time()
+tempPred, tempMetricas = execution(temperatureModel, gaussianLoss, tempData, 
+                                  tempStore, tempTime, tempParams)
+
+fin2 = timeMod.time()
+timecalc(start2, fin2, f'Tiempo para el modelo de temperatura ({tempParams[0]} epochs, batch size de {tempParams[1]} y {tempParams[2]} repeticiones)', True)
+bot_texter(f'Medianas: {np.median(tempMetricas, axis=1)}')
+
+# temperature boxplot
+tCols = ['Mean Bias', 'P2 Bias', 'P98 Bias', 'R (Pearson)', 'std Ratio', 'RMSE', 'Bias WAMS', 'Bias CAMS']
+
+boxplots(tempMetricas, tCols, 
+         ['Temperature', f'./Resultados/Temperatura/plots/{tempParams[0]}_epochsTempMetrics.pdf'])
+
+# temperature animation map (over 2017)
+animation(tempPred - 273.15, [np.min(tLong), np.max(tLong), np.min(tLat), np.max(tLat)], 
+          't', tempTime[1][2546:2920], 365)
+
+# temperature metrics maps
+for i in range(len(tCols)):
+    mapeo(tempMetricas, [np.shape(tempMetricas)[0], 68, 158], 
+      [np.min(tLong), np.max(tLong), np.min(tLat), np.max(tLat)], 
+      [get_colormap('tm'), f'./Resultados/Temperatura/plots/tempMetricsMap{i}.pdf'], 
+      [0, i])
+"""
+
+
+# PRECIPITATION
+precipParams = [100, 16, 4]
+
+# precipitation data (without and with a sea mask respectively)
+precipData, precipStore, precipTime = extractData(era5data, precip - 1, times, "Precipitation")
+precipData2 = [precipData[0], extractMask(precip2[:times[1]]), precipData[2], extractMask(precip2[times[1]:])]
+
+
+
+# precipitation results (using only values over the islands)
+start4 = timeMod.time()
+precipPred, precipMetricas = execution(precipModel, bernouilliGammaLoss, 
+                                       precipData2, precipStore, precipTime, 
+                                       precipParams)
+
+fin4 = timeMod.time()
+timecalc(start4, fin4, f'Tiempo para el modelo de precipitaciones ({precipParams[0]} epochs, batch size de {precipParams[1]} y {precipParams[2]} repeticiones)', True)
+
+bot_texter(f'Medianas: {np.median(precipMetricas, axis=1)}')
+
+# fill the complete grid with predicted values for islands
+precipData3 = np.reshape(precipData[3], (np.shape(precipData[3])[0], 68*158))
+reMask = np.reshape(precip2[times[1]:] >= 0, (np.shape(precip2[times[1]:] >= 0)[0], 68*158))
+
+printeo('Construyendo resultados...')
+
+precipPredValues = unmask(precipPred, np.where(reMask == False, 0, -999.))
+precipMetricsValues = unmask(precipMetricas, np.where(reMask[:6, :] == False, 0, -999.))
+
+# precipitation boxplots
+pCols = ['Mean Bias', 'P98 Bias', 'R (Spearman)', 'RMSE (Wet days)', 'Bias WetAMS', 'Bias_DryAMS']
+
+boxplots(precipMetricas[:2, :], pCols[:2], 
+         ['biases', f'./Resultados/Precipitacion/plots/(mask){precipParams[0]}_epochsPrecipMetrics1.pdf'])
+
+boxplots(precipMetricas[2:4, :], pCols[2:4], 
+         ['Precipitation', f'./Resultados/Precipitacion/plots/(mask){precipParams[0]}_epochsPrecipMetrics2.pdf'])
+
+boxplots(precipMetricas[4:, :], pCols[4:], 
+         ['Precipitation', f'./Resultados/Precipitacion/plots/(mask){precipParams[0]}_epochsPrecipMetrics3.pdf'])
+
+printeo('Sacando fotogramas...')
+
+# precipitation animation map (over 2017)
+animation(precipPredValues, [np.min(pLong), np.max(pLong), np.min(pLat), np.max(pLat)], 
+          'p', precipTime[1][2546:2920], 365)
+
+# precipitation metrics maps
+for i in range(len(pCols)):
+    mapeo(precipMetricsValues, [np.shape(precipMetricsValues)[0], 68, 158], 
+      [np.min(pLong), np.max(pLong), np.min(pLat), np.max(pLat)], 
+      [get_colormap('pm'), f'./Resultados/Precipitacion/plots/(mask)precipMetricsMap{i}.pdf'], 
+      [0, i], True)
---
+++ python/nets.py
@@ -0,0 +1,207 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+
+def temperatureModel(training, labels, lossfunc, params):
+    """
+    Temperature model. Contains an input layer shaped as the input's shape; 
+    four hidden layers (three 2D convolutional and a flatten); an output layer 
+    composed by the concatenation of two dense layers. 
+
+    Parameters
+    ----------
+    training : np.ndarray
+        Training data, of shape (time, latitude, longitude, geo-height)
+    labels : np.ndarray
+        Labels for the training data, of shape (time, outputs). 
+        
+    Raises
+    ------
+    TypeError:
+        - If arguments are not NumPy arrays.
+        - If arguments are not the same length (temporal dimension).
+
+    Returns
+    -------
+    model : keras.src.engine.functional.Functional
+        CNN model 
+
+    """
+    
+    if not all(isinstance(i, np.ndarray) for i in [training, labels]):
+        raise TypeError("args must be NumPy arrays")
+    
+    if np.shape(training)[0] != np.shape(labels)[0]:
+        raise TypeError("training and labels must be the same length")
+        
+    if not isinstance(params, list) and not all(isinstance(i, int) for i in params):
+        raise TypeError("params must be a list of two integers")
+    
+    
+    # Input layer
+    input_shape=(np.shape(training)[1], np.shape(training)[2], np.shape(training)[3])
+    inputs = tf.keras.layers.Input(shape=input_shape)
+
+    # Hidden layers
+    l1 = tf.keras.layers.Conv2D(filters=50, kernel_size=(3, 3), activation='relu', padding='valid')(inputs)
+    l2 = tf.keras.layers.Conv2D(filters=25, kernel_size=(3, 3), activation='relu', padding='valid')(l1)
+    l3 = tf.keras.layers.Conv2D(filters=10, kernel_size=(3, 3), activation='relu', padding='valid')(l2)
+    l4 = tf.keras.layers.Flatten()(l3)
+
+    # Output layer
+    l51 = tf.keras.layers.Dense(units=np.shape(labels)[1], activation='linear')(l4)
+    l52 = tf.keras.layers.Dense(units=np.shape(labels)[1], activation='linear')(l4)
+    outputs = tf.keras.layers.Concatenate()([l51, l52])
+
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001), loss=lossfunc)
+    model.summary()
+
+    #train and store the data
+    history = model.fit(training, labels, validation_split=0.1, 
+                    epochs=params[0], batch_size=params[1], verbose=1)
+    
+    return model, history 
+
+
+
+
+
+def precipModel(training, labels, lossfunc, params):
+    """
+    Precipitation model. Contains an input layer shaped as the input's shape; 
+    four hidden layers (three 2D convolutional and a flatten); an output layer 
+    composed by the concatenation of three dense layers. 
+
+    Parameters
+    ----------
+    training : np.ndarray
+        Data to train the model with.
+    labels : np.ndarray
+        Labels of the data.
+        
+    Raises
+    ------
+    TypeError:
+        - If arguments are not NumPy arrays.
+        - If arguments are not the same length (temporal dimension).
+
+    Returns
+    -------
+    model : keras.src.engine.training.Model
+        CNN model 
+
+    """
+    
+    if not all(isinstance(i, np.ndarray) for i in [training, labels]):
+        raise TypeError("args must be NumPy arrays")
+    
+    if np.shape(training)[0] != np.shape(labels)[0]:
+        raise TypeError("training and labels must be the same length")
+        
+    if not isinstance(params, list) and not all(isinstance(i, int) for i in params):
+        raise TypeError("params must be a list of two integers")
+    
+    # Input layer
+    input_shape=(np.shape(training)[1], np.shape(training)[2], np.shape(training)[3])
+    inputs = tf.keras.layers.Input(shape=input_shape)
+
+    # Hidden layers
+    l1 = tf.keras.layers.Conv2D(filters=50, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+    l2 = tf.keras.layers.Conv2D(filters=25, kernel_size=(3, 3), activation='relu', padding='same')(l1)
+    l3 = tf.keras.layers.Conv2D(filters=1, kernel_size=(3, 3), activation='relu', padding='same')(l2)
+    l4 = tf.keras.layers.Flatten()(l3)
+
+    # Output layer
+    l51 = tf.keras.layers.Dense(units=np.shape(labels)[1], activation='sigmoid')(l4)
+    l52 = tf.keras.layers.Dense(units=np.shape(labels)[1], activation='linear')(l4)
+    l53 = tf.keras.layers.Dense(units=np.shape(labels)[1], activation='linear')(l4)
+    outputs = tf.keras.layers.Concatenate()([l51, l52, l53])
+
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001), loss=lossfunc)
+    model.summary()
+
+    #train and store the data
+    history = model.fit(training, labels, validation_split=0.1, 
+                    epochs=params[0], batch_size=params[1], verbose=1)
+    
+    results = model.predict(training)
+
+    return model, history, results[:, :np.shape(labels)[1]]
+
+
+
+
+
+# only for dense layers output !!!!!
+def gaussianLoss(true, pred): 
+    """
+    Optimize the negative log-likelihood of the Gaussian distribution.
+
+    Parameters
+    ----------
+    true : np.ndarray
+        True values.
+    pred : np.ndarray
+        Predicted values.
+
+    Returns
+    -------
+    loss : tf.python.framework.ops.EagerTensor
+        Loss to be minimized.
+
+    """
+
+    #d = int(K.int_shape(pred)[1]/2)
+    d = int(pred.shape[1]/2)
+    media = pred[:, :d]
+    log_var = pred[:, d:]
+    precision = tf.exp(-log_var)
+    loss = tf.reduce_mean(0.5 * precision * (true - media)**2 + 0.5 * log_var)
+    
+    return loss
+
+
+
+
+
+def bernouilliGammaLoss(true, pred):
+    """
+    Optimize the negative log-likelihood of the Bernouilli-Gamma distribution.
+
+    Parameters
+    ----------
+    true : np.ndarray
+        True values.
+    pred : np.ndarray
+        Predicted values.
+
+    Returns
+    -------
+    loss : tf.python.framework.ops.EagerTensor
+        Loss to be minimized.
+
+    """
+    
+    #d = int(K.int_shape(pred)[1]/3)
+    d = int(pred.shape[1]/3)
+    ocurrence = pred[:, :d]
+    shape_parameter = tf.exp(pred[:, d:2*d])
+    scale_parameter = tf.exp(pred[:,2*d:])
+    bool_rain = tf.cast(tf.math.greater(true, 0), dtype=tf.float32) 
+    epsilon = 0.000001 #avoid indeterminations
+    loss = -tf.reduce_mean((1-bool_rain)*tf.math.log(1 - ocurrence + epsilon) + 
+                           bool_rain*(tf.math.log(ocurrence+epsilon) 
+                                      + (shape_parameter - 1)*tf.math.log(true + epsilon) 
+                                      - shape_parameter*tf.math.log(scale_parameter + epsilon)
+                                      - tf.math.lgamma(shape_parameter + epsilon) 
+                                      - true/(scale_parameter + epsilon))) #logarithm of the b-g loss
+    
+    return loss
+
+
+
